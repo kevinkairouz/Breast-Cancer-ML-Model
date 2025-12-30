daf42a1b0887b3fcfe5e4b4c302a04c60c70aeb0
kevinkairouz <KM.Kairouz@gmail.com>
2025-12-30 17:37:50 -0500

    first acc test completed

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
 import matplotlib as mpl
-import pandas as pd 
+import pandas as pd  
+import numpy as np
 from sklearn.model_selection import train_test_split 
-from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.neighbors import KNeighborsClassifier  
+from sklearn.preprocessing import StandardScaler
 
 if __name__ == "__main__": 
 
@@ -12,8 +14,23 @@ if __name__ == "__main__":
 
     #print(df.groupby("diagnosis")["diagnosis"].count()) #to see ratio of postiive & negative cases 
 
-    Y = df["diagnosis"] 
+    Y = df["diagnosis"].to_numpy()
     X = df.drop("diagnosis", axis=1) 
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, stratify=Y)  
+
+
+    
+
+    model = KNeighborsClassifier() 
+
+    model.fit(X_train, Y_train) 
+
+    acc_score = model.score(X_test, Y_test) 
+    print(acc_score) 
+
+
+
 
      
 
