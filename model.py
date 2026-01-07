d5f6a8dae1a841ad0a38c5c06173812b0482879a
kevinkairouz <KM.Kairouz@gmail.com>
2026-01-07 01:46:34 -0500

    added comments

--- model.py
+++ model.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as mpl
 import pandas as pd  
-import numpy as np
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import classification_report 
 from sklearn.linear_model import LinearRegression 
---
+++ test.py
@@ -0,0 +1,4 @@
+#used for algorithm analysis measuring effectiveness of each algorithm that was used 
+#and algorithm that i used  
+
+import matplotlib.pyplot as mpl 
