9fab01db223b8609f78cf20b4e67c1795c6e8137
kevinkairouz <KM.Kairouz@gmail.com>
2026-01-08 00:56:58 -0500

    created grids and split data

--- analysis.py
+++ analysis.py
@@ -7,9 +7,32 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier 
 from sklearn.neighbors import KNeighborsClassifier 
 from sklearn.metrics import classification_report 
-from sklearn.model_selection import GridSearchCV 
+from sklearn.model_selection import GridSearchCV  
+from sklearn.model_selection import train_test_split
+import numpy as np 
+
+
+#TODO  
+"""
+1) compare the precision and recall among the models and plot it on bar chart 
+2) compare the score/accraucy and plot it on the bar chart  
+3) if possible compare each model variation using subplots of the precision, recall, score/accuracy
+"""
 
 if __name__ == "__main__": 
+
+    scores = np.array([])  
+
+    df = pd.read_csv("breast-cancer.csv")  
+    df = df.replace("M", "1") 
+    df = df.replace("B", "0") 
+    df = df.astype({"diagnosis": int}) 
+    df = df.drop("id", axis = 1)
+
+    Y = df[["diagnosis"]] 
+    X = df.drop("diagnosis", axis = 1) 
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, stratify=Y, random_state=42) 
 
     RF = RandomForestClassifier()
     DT = DecisionTreeClassifier()
@@ -19,9 +42,18 @@ if __name__ == "__main__":
     RF_params = {"n_estimators": [1,10,100,500,700,1000], "max_depth": [1,5,10,15,20,30]} 
     DT_params = {"max_depth": [1,5,10,15,20,30]} 
     KN_params = {"n_neighbors": [5,10,15,20,25]}  
-    LR_params = {"C": [1,5,15,50,100]}  
+    LR_params = {"C": [1,5,15,50,100]} 
 
-    RF_grid = GridSearchCV
+
+    RF_model = GridSearchCV(RF, RF_params) 
+    
+
+
+
+
+
+    
+
 
 
  
--- model.py
+++ model.py
@@ -1,9 +1,11 @@
+#using the model that performed the best 
+#printing out its score to the console 
+
 import pandas as pd  
 from sklearn.model_selection import train_test_split 
-from sklearn.metrics import classification_report 
-
 from sklearn.preprocessing import StandardScaler 
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV 
+from flask import Flask 
 
 
 if __name__ == "__main__": 
@@ -13,6 +15,9 @@ if __name__ == "__main__":
     df = df.replace("B", "0") 
     df = df.astype({"diagnosis": int}) 
     df = df.drop("id", axis = 1)  
+
+    # print(df.groupby("diagnosis")["diagnosis"].count()) done to see amount of M & B 
+
 
 
     # print(df.info()) done for testing purpose
