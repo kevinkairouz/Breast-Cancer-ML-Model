8b0ea516b2f60c22c1fdc45ed085f7a712ded2f7
kevinkairouz <KM.Kairouz@gmail.com>
2026-01-07 22:59:31 -0500

    deleted test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-#used for algorithm analysis measuring effectiveness of each algorithm that was used 
-#and algorithm that i used  
-import matplotlib.pyplot as mpl  
-from sklearn.linear_model import LogisticRegression 
-from sklearn.ensemble import RandomForestClassifier 
-from sklearn.tree import DecisionTreeClassifier 
-from sklearn.neighbors import KNeighborsClassifier 
-from sklearn.metrics import classification_report 
