6fbfe12bb435a46238593eba625dc794ad9f61cf
kevinkairouz <KM.Kairouz@gmail.com>
2026-01-08 22:06:02 -0500

    added model

--- model/model.py
+++ model/model.py
@@ -1,48 +1,46 @@
 #using the model that performed the best 
-#printing out its score to the console  
-
-#importing analysis will allow me to use the most_accurate model and then print out its score 
-#and then allow for me to host it on api  
-
-#then can use html and css and js to give interactiveness and or console interactiveness 
-
-
 import pandas as pd   
 import analysis 
-from sklearn.model_selection import train_test_split 
-from sklearn.preprocessing import StandardScaler 
-from sklearn.model_selection import GridSearchCV 
+from sklearn.model_selection import train_test_split  
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import recall_score 
 from flask import Flask, jsonify, request   
 import sqlite3 as sql 
 
+#TODO: 
+"""
+1) create api and have it work with the prediction function 
+2) make sure that at the end of the function in python we write to our sqlite3 database 
+3) js/html/css to finish and deploy/test
+"""
 
 
 
 
+def main():
 
-
-
-
-
-if __name__ == "__main__": 
-
-    df = pd.read_csv("model/breast-cancer.csv") 
+    model = RandomForestClassifier(random_state=42, n_estimators= 100, max_depth=15) 
+    
+    df = pd.read_csv("breast-cancer.csv")  
     df = df.replace("M", "1") 
     df = df.replace("B", "0") 
     df = df.astype({"diagnosis": int}) 
-    df = df.drop("id", axis = 1)  
+    df = df.drop("id", axis = 1)
 
-    # print(df.groupby("diagnosis")["diagnosis"].count()) done to see amount of M & B 
+    Y = df["diagnosis"].to_numpy()
+    X = df.drop("diagnosis", axis = 1).to_numpy()  
 
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, stratify=Y, random_state=42) 
 
-
-    # print(df.info()) done for testing purpose
-
-
-
-
+    model.fit(X_train, Y_train)
+    Y_pred = model.predict(X_test) 
+    recall = recall_score(Y_test, Y_pred)
+    print(f"Model Accuracy is {model.score(X_test, Y_test)} with a recall of {recall}")
     
+
+    # Y_user_predicted = model.predict(X_input)
+    # return Y_user_predicted
 
      
 
-    
+main() 
