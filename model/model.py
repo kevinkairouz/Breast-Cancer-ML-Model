7edac5f9a8e2476d49ec3ffae98e6f1f739a88c0
kevinkairouz <KM.Kairouz@gmail.com>
2026-01-09 22:11:55 -0500

    removed comments

--- model/model.py
+++ model/model.py
@@ -1,21 +1,8 @@
-#using the model that performed the best 
 import pandas as pd   
 import analysis 
 from sklearn.model_selection import train_test_split  
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import recall_score 
-from flask import Flask, jsonify, request   
-import sqlite3 as sql 
-
-#TODO: 
-"""
-1) create api and have it work with the prediction function 
-2) make sure that at the end of the function in python we write to our sqlite3 database 
-3) js/html/css to finish and deploy/test
-"""
-
-
-
 
 def main():
 
@@ -36,11 +23,5 @@ def main():
     Y_pred = model.predict(X_test) 
     recall = recall_score(Y_test, Y_pred)
     print(f"Model Accuracy is {model.score(X_test, Y_test)} with a recall of {recall}")
-    
-
-    # Y_user_predicted = model.predict(X_input)
-    # return Y_user_predicted
-
-     
 
 main() 
